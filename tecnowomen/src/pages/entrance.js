f2555ef54e319cb82900edb6f1764f371e6b16b7
pricaraujo <60271605+pricaraujo@users.noreply.github.com>
2023-03-17 13:58:41 -0300

    Apaga pasta de src

--- tecnowomen/src/pages/entrance.js
+++
@@ -1,8 +0,0 @@
-function topNavbar() {
-    var x = document.getElementById("topNav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  } 
