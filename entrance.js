7543136abaedcca8fac7a48b18c03721d7775a67
pricaraujo <60271605+pricaraujo@users.noreply.github.com>
2023-03-17 14:17:19 -0300

    Adiciona função alert

--- entrance.js
+++ entrance.js
@@ -1,8 +1,12 @@
 function topNavbar() {
     var x = document.getElementById("topNav");
     if (x.className === "topnav") {
-      x.className += " responsive";
+      x.className += "responsive";
     } else {
       x.className = "topnav";
     }
   } 
+
+  function alertFunction(){
+    alert("Esta página virá na versão 2.0!")
+  }
